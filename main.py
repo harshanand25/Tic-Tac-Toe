53c3171b2c4dbced3b3a54531b1f32439735c963
Harsh <harsh250600@gmail.com>
2022-12-13 21:57:25 +0530

    project completed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+def sum(a,b,c):
+    return a+b+c
 
 def printBoard(xState, zState):
     zero = 'X' if xState[0] else ('O' if zState[0] else 0)
@@ -16,9 +18,20 @@ def printBoard(xState, zState):
     print(f"--|---|--")
     print(f"{six} | {seven} | {eight}")
 
+def checkWin(xState, zState):
+    wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    for win in wins:
+        if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3):
+            print("X won the match")
+            return 1
+        if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3):
+            print("O won the match")
+            return 0
+    return -1
+
 if __name__ == "__main__":
-    xState = [0,0,0,0,0,0,0,0]
-    zState = [0,0,0,0,0,0,0,0]
+    xState = [0,0,0,0,0,0,0,0,0]
+    zState = [0,0,0,0,0,0,0,0,0]
 
     turn = 1 # 1 for x and 0 for o
     print("Welcome to Tic Tac Toe")
@@ -32,3 +45,9 @@ if __name__ == "__main__":
             print("O's Chance")
             value = int(input("Please enter a value"))
             zState[value] = 1
+        cwin = checkWin(xState, zState)
+        if(cwin != -1):
+            print("Match Over")
+            break
+
+        turn = 1 - turn
