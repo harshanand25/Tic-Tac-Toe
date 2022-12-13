d98c159b52d6d66ef03b2e0c5c0c29463cb17a2e
Harsh <harsh250600@gmail.com>
2022-12-13 21:27:41 +0530

    hello

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 
 def printBoard(xState, zState):
-    print(f"0 | 1 | 2")
+    zero = 'X' if xState[0] else ('O' if zState[0] else 0)
+    one = 'X' if xState[1] else ('O' if zState[1] else 1)
+    two = 'X' if xState[2] else ('O' if zState[2] else 2)
+    three = 'X' if xState[3] else ('O' if zState[3] else 3)
+    four = 'X' if xState[4] else ('O' if zState[4] else 3)
+    five = 'X' if xState[5] else ('O' if zState[5] else 5)
+    six = 'X' if xState[6] else ('O' if zState[6] else 6)
+    seven = 'X' if xState[7] else ('O' if zState[7] else 7)
+    eight = 'X' if xState[8] else ('O' if zState[8] else 8)
+
+    print(f"{zero} | {one} | {two}")
     print(f"--|---|--")
-    print(f"3 | 4 | 5")
+    print(f"{three} | {four} | {five}")
     print(f"--|---|--")
-    print(f"6 | 7 | 8")
-    pass
+    print(f"{six} | {seven} | {eight}")
 
 if __name__ == "__main__":
     xState = [0,0,0,0,0,0,0,0]
@@ -18,9 +27,8 @@ if __name__ == "__main__":
         if(turn==1):
             print("X's Chance")
             value = int(input("Please enter a value"))
-            zState[value] = 1
+            xState[value] = 1
         else:
-            print("X's Chance")
+            print("O's Chance")
             value = int(input("Please enter a value"))
             zState[value] = 1
-        break
